656e0d77be1152982f46126930d787ccde33b5e4
emoeller <luminarada80@gmail.com>
2024-10-18 09:47:16 -0400

    Added code to analyze_mESC.py to find the number of TFs and TGs in the ground truth data and find the number of shared TFs between the dataset and the ground truth file. Created Step_055.Slurm_Cell_Type_Model.sh to submit the Homer motif matching and cell-type specific TRN inference as an sbatch script

--- analyze_mESC.py
+++ analyze_mESC.py
@@ -6,14 +6,20 @@ adata_RNA = sc.read_h5ad('./data/adata_RNA.h5ad')
 adata_ATAC = sc.read_h5ad('./data/adata_ATAC.h5ad')
 ground_truth = pd.read_csv('./data/filtered_ground_truth_56TFs_3036TGs.csv')
 
-# print(adata_RNA)
-# print(adata_ATAC)
-
-# print(adata_RNA.var['gene_ids'])
-
-print(ground_truth['Source'])
+ground_truth_genes = []
+adata_RNA_genes = []
 
 for gene in ground_truth['Source']:
-    if gene in ground_truth.var['gene_ids']:
-        print(gene)
+    if gene not in ground_truth_genes:
+        ground_truth_genes.append(gene.upper())
+
+for gene in adata_RNA.var['gene_ids']:
+    if gene not in adata_RNA_genes:
+        adata_RNA_genes.append(gene.upper())
+
+print(len(set(ground_truth['Source'])))
+print(len(set(ground_truth['Target'])))
+print(len(ground_truth['Target']))
+
+print(f'Common Genes between adata_RNA and ground truth: {len(list(set(ground_truth_genes) & set(adata_RNA_genes)))}')
 
